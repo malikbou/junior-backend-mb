2b6f956e66df09cca21adf231d19026f8577f5b4
Malik Bouaoudia <malik@bouaoudia.com>
2023-03-22 11:05:57 +0000

    got routing to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,17 @@ require 'uri'
 
 class PagesController < ApplicationController
   def home
+
+  end
+
+  def museums
     api_key = ENV["MAPBOX_API_KEY"]
-    @latitude = 52.494857
-    @longitude = 13.437641
+    # @latitude = 52.494857
+    # @longitude = 13.437641
+    @latitude = params[:lat]
+    @longitude = params[:long]
+    @member = params[:member]
+
     url = URI("https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{@longitude},#{@latitude}&access_token=#{api_key}")
     response = Net::HTTP.get(url)
     @json = JSON.parse(response)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "museums", to: "pages#museums", as: :museums
 end
